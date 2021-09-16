c05beaf441d835cd923ef61562f7d2d63f02db1a
spoonya <nomad8t@gmail.com>
2021-09-16 09:34:30 +0300

    fix: error

--- src/scripts/filters/catalog/slider.js
+++ src/scripts/filters/catalog/slider.js
@@ -3,6 +3,8 @@ import noUiSlider from 'nouislider';
 const slider = document.querySelector('#filter-slider');
 
 function createFilterSlider() {
+  if (!slider) return;
+
   noUiSlider.create(slider, {
     start: [1, 12],
     connect: true,
