5428ac3d364d1094fbbf6a68294d54b2c807a74a
spoonya <nomad8t@gmail.com>
2021-09-17 16:56:43 +0300

    refactor: remove images

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,8 @@ import 'core-js';
 import 'regenerator-runtime/runtime';
 
 import controlAccordion from './accordion';
-
 // eslint-disable-next-line no-unused-vars
 import * as sliders from './sliders';
-
 import {
   selectActiveFilter,
   createFilterSlider,
@@ -13,12 +11,12 @@ import {
   calcActiveFilters,
   controlFiltersBurger
 } from './filters';
-
 import { validateFormCart } from './forms';
 import controlModal from './modal';
 import { controlHeaderBurger } from './header';
 
 controlAccordion();
+
 selectActiveFilter();
 createFilterSlider();
 resetFiltersOnClick();
